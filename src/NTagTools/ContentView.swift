76cce01ea2ad5efe1157c07c66e6637a2b973c24
岡優志 <yu.j.0513@gmail.com>
2024-09-05 23:58:11 +0900

    新規プロジェクト追加

---
+++ src/NTagTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NTagTools
+//
+//  Created by yuji on 2024/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/NTagTools/NTagToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTagToolsApp.swift
+//  NTagTools
+//
+//  Created by yuji on 2024/09/05.
+//
+
+import SwiftUI
+
+@main
+struct NTagToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
