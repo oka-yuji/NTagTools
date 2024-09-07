036916e11e22823fc6e0cd1e1b1d74ecf63a4c8c
岡優志 <yu.j.0513@gmail.com>
2024-09-08 04:22:48 +0900

    SwiftLintとFormatを追加

--- src/NTagToolsTests/NTagToolsTests.swift
+++ src/NTagToolsTests/NTagToolsTests.swift
@@ -5,11 +5,10 @@
 //  Created by yuji on 2024/09/05.
 //
 
-import XCTest
 @testable import NTagTools
+import XCTest
 
 final class NTagToolsTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class NTagToolsTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- src/NTagToolsUITests/NTagToolsUITestsLaunchTests.swift
+++ src/NTagToolsUITests/NTagToolsUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class NTagToolsUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
